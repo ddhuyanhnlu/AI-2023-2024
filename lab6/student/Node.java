68d14c00d213cbb83fded9a76ce5385f34518576
ddhuyanhnlu <128769522+ddhuyanhnlu@users.noreply.github.com>
2023-11-16 21:36:33 +0700

    Add files via upload

---
+++ lab6/student/Node.java
@@ -0,0 +1,99 @@
+package student;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+
+public class Node {
+	public static final int N = 8;
+	private Queen[] state;
+
+	public Node() {
+		// generateBoard();
+		state = new Queen[N];
+	}
+
+	public Node(Queen[] state) {
+		this.state = new Queen[N];
+		for (int i = 0; i < state.length; i++) {
+			this.state[i] = new Queen(state[i].getRow(), state[i].getColumn());
+		}
+	}
+	
+	public Node getBestCandidate(){
+		return null;
+		
+	}
+
+	public Node(Node n) {
+		this.state = new Queen[N];
+		for (int i = 0; i < N; i++) {
+			Queen qi = n.state[i];
+			this.state[i] = new Queen(qi.getRow(), qi.getColumn());
+		}
+	}
+
+	public void generateBoard() {
+		Random random = new Random();
+		for (int i = 0; i < N; i++) {
+			state[i] = new Queen(random.nextInt(N), i);
+		}
+	}
+
+	public int getH() {
+		int heuristic = 0;
+		// Enter your code here
+		for(int i =0; i < N;i++) {
+			for(int j = i +1; j < N; j++) {
+				if(state[i].isConflict(state[i])) {
+					heuristic++;
+				}
+			}		
+		}
+		return heuristic;
+	}
+
+	public List<Node> generateAllCandidates() {
+		List<Node> result = new ArrayList<Node>();
+
+		// Enter your code here
+		for (int i = 0; i < N; i++) {
+			Node tmp = new Node(this.state);
+			tmp.state[i].move();
+			result.add(tmp);
+		}
+		return result;
+	}
+
+	public Node selectNextRandomCandidate() {
+		// Enter your code here
+		Node re = new Node(this.state);
+		Random r = new Random();
+		int i = r.nextInt(N);//[0->7]
+		int row = r.nextInt(N);//[0->7]
+		re.state[i].setRow(row);
+		
+		return re;
+	}
+
+	public void displayBoard() {
+		int[][] board = new int[N][N];
+		// set queen position on the board
+		for (int i = 0; i < N; i++) {
+			board[state[i].getRow()][state[i].getColumn()] = 1;
+		}
+		// print board
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (board[i][j] == 1) {
+					System.out.print("Q" + " ");
+				} else {
+					System.out.print("-" + " ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
